61f6af7e6e3ed84856c52d164095f35334ed93e1
Unknown <saif.kermoun@gmail.com>
2018-04-10 01:24:10 +0200

    Add material angular 5 #2
    step 2

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import {ChartsModule} from "ng2-charts";
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
@@ -18,6 +18,7 @@ import { GlobalStatComponent } from './components/global-stat/global-stat.compon
 import { SearchPlayerComponent } from './components/search-player/search-player.component';
 import { MatchHistoryComponent } from './components/match-history/match-history.component';
 import { ActualSeasonComponent } from './components/actual-season/actual-season.component';
+
 
 @NgModule({
   declarations: [
@@ -37,8 +38,9 @@ import { ActualSeasonComponent } from './components/actual-season/actual-season.
     MDBBootstrapModule,
     routes,
     CommonModule,
-    BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    BrowserAnimationsModule ,
+    ToastrModule.forRoot(),
+    HomeComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [StatsService, ErrorService],
--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
-import {Component} from '@angular/core';
-import { OnInit } from '@angular/core';
+import {Component, NgModule, OnInit } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material';
+
+@NgModule({
+  imports : [MatButtonModule],
+  exports : [MatButtonModule],
+})
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
@@ -23,39 +29,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() { }
 
-
-  /*getGeneralOnlyStat(username) {
-    this.statS.getStat('pc', username).subscribe(
-        res => {
-          this.$resStat = res;
-          if (!this.$resStat.error) {
-            this.$user = {
-              accountId : res.accountId,
-              platformID: res.accountId,
-              platformName : res.platformName,
-              platformNameLong : res.platformNameLong,
-              epicUserHandle : res.epicUserHandle
-            };
-            this.$lifeTimeStats = {
-              totWin : res.lifeTimeStats[8].value,
-              totMatches : res.lifeTimeStats[7].value,
-              totKill : res.lifeTimeStats[10].value,
-              totKD : res.lifeTimeStats[11].value,
-              totWPerc : res.lifeTimeStats[9].value,
-              totScore : res.lifeTimeStats[6].value,
-              timePlayed : res.lifeTimeStats[13].value,
-            };
-            this.$SoloGame = this.$resStat.stats['p2'];
-            this.$SquadGame = this.$resStat.stats['p9'];
-            this.$DuoGame = this.$resStat.stats['p10'];
-          } else {
-            this.$resStat = res;
-          }
-        },
-        err => this.errors.handleError('Erreur', 'Une erreur est survenue.'),
-        //() => console.log()
-    )
-  };*/
 
   getFormData(payload) {
     setTimeout(() => this.readyToCharge = true);
